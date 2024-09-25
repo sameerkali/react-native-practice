dd511042110e5bc52268473b9c6e13dafed3f73f
sameerkali <s.faridi007@gmail.com>
2024-09-25 22:45:23 +0530

    stack navigator

---
+++ app/_layout.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const _layout = () => {
+  return (
+  <Stack>
+    <Stack.Screen name="index" />
+    <Stack.Screen name="settings" />
+  </Stack>
+  )
+}
+
+export default _layout
+
+const styles = StyleSheet.create({
+    home: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center"
+    }
+  });

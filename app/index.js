11f9a8531472e8d83dbab052364dc41e012b0873
sameerkali <s.faridi007@gmail.com>
2024-09-25 22:25:06 +0530

    initial setup basic routing with Link

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+
+const index = () => {
+  return (
+    <View style={styles.home}>
+      <Text>I am index</Text>
+      <Link href={`/settings`}><Text>open settings</Text></Link>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({
+  home: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ app/settings.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+const settings = () => {
+  return (
+    <View style={styles.home}>
+      <Text> I am settings</Text>
+     <Link href={'/'}>home</Link>
+    </View>
+  );
+};
+
+export default settings;
+
+const styles = StyleSheet.create({
+  home: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
